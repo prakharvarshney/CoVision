6fda5d8c046189b71afbdfa0cefe2c962abd7cde
Prakhar Varshney <prakharinvertis@gmail.com>
2020-04-26 19:16:51 +0530

    Add files via upload

---
+++ Client/src/Components/Modals/ModalResultPage.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons'
+
+function ModalResultPage(){
+  return(
+    <Container>
+      ModalResultPage
+    </Container>
+  )
+}
+
+export default ModalResultPage;
